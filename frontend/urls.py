9eded8dd9705f151b7b3cf76297b40eb6f38c0fb
dittko <wiktorm205@gmail.com>
2025-01-12 14:03:44 +0100

    dodanie widoku prototypu admina

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,8 @@
+# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Renderowanie strony głównej
-    path('admin.html', views.admin_dashboard, name='admin_dashboard'),  # Strona administracyjna
+    path('adminmars/', views.admin_dashboard, name='admin_dashboard'), 
+    path('', views.home, name='home'),  
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,8 @@
+# views.py
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')  # Strona główna
+    return render(request, 'index.html') 
 
 def admin_dashboard(request):
-    # Przykład: sprawdź, czy użytkownik jest zalogowany jako admin
-    if request.user.is_authenticated and request.user.username == 'admin':
-        return render(request, 'admin.html')  # Strona administratora
-    else:
-        return render(request, 'index.html')  # Jeśli nie jest adminem, wróć do strony głównej
+    return render(request, 'admin.html')  
