ce0bd00687a6ce54d78ce85572d2df61800a80fc
Maciej Kuczynski <kuczynskimaciej1@poczta.onet.pl>
2025-01-18 01:19:02 +0100

    DB migration, tables created, file tree fix

--- marsdust/settings.py
+++ marsdust/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.frontend',
     'marsdust',
-    'rest_framework'
+    'rest_framework',
+    'db',
+    'services'
 ]
 
 MIDDLEWARE = [
