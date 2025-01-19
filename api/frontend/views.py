c3baba8d595350cf5694eef19a31ba83d8efa645
dittko <wiktorm205@gmail.com>
2025-01-19 13:40:38 +0100

    dodanie informacji na temat projektu

--- api/frontend/views.py
+++ api/frontend/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')  
+    return render(request, 'index.html')
 
 def admin_dashboard(request):
     if request.user.is_authenticated and request.user.username == 'admin':
-        return render(request, 'admin.html')  
+        return render(request, 'admin.html')
     else:
-        return render(request, 'index.html')  
+        return render(request, 'index.html')
 
 def gallery(request):
-    return render(request, 'gallery.html')  
+    return render(request, 'gallery.html')
+
+def information(request):
+    return render(request, 'information.html')  
